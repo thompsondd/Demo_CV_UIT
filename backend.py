e8cd1c5a964611f5c90a26a1345fd74091ab33d0
DangPC <dnng2002@gmail.com>
2022-12-20 10:12:22 +0700

    Update new model

--- backend.py
+++ backend.py
@@ -5,25 +5,28 @@ import cv2 as cv
 from skimage import feature
 import pandas as pd
 import os
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras import Model
 
 model_folder = r"./model_and_encoder"
 
 def get_model_path(model):
     return os.path.join(model_folder,model)
-SVM_hist_path = "squared_hinge_l2_LinearSVC_hist.pkl"
+SVM_hist_path = "hinge_l2_10_LinearSVC_hist.pkl"
 Softmax_hist_path = "multinomial_lbfgs_LogisticRegression_hist.pkl"
 
-Softmax_hog_path = "multinomial_lbfgs_LogisticRegression_hog.pkl"
-SVM_hog_path = "hinge_l2_LinearSVC_hog.pkl"
+Softmax_hog_path = "multinomial_sag_LogisticRegression_hog.pkl"
+SVM_hog_path = "squared_hinge_l2_1LinearSVC_hog.pkl"
 
-SVM_vgg16_path = "vgg16_LinearSVM_l2_hinge.sav"
-Softmax_vgg16_path = "vgg16_softmax.sav"
+SVM_vgg16_path = "VGG16_LinearSVC_l2_hinge.sav"
+Softmax_vgg16_path = "VGG16_softmax_lbfgs.sav"
 
-SVM_vgg19_path = "vgg19_LinearSVM_l2_hinge.sav"
-Softmax_vgg19_path = "vgg19_softmax.sav"
+SVM_vgg19_path = "VGG19_LinearSVC_l2_hinge.sav"
+Softmax_vgg19_path = "VGG19_softmax_lbfgs.sav"
 
-encoder_hog = "encoder_HOG.pkl"
-encoder_hist = "encoder_hist.pkl"
+encoder_ = "encoder_hog.pkl"
 
 def calHist(img):
     hist = cv.calcHist([img],[0],None, [256],[0,256])
@@ -32,22 +35,23 @@ def calHist(img):
     return hist
 
 def calHOG(img):
-    img =cv.resize(img, (128,256))
+    img =cv.resize(img, (256,128))
     (hog, hog_image) = feature.hog(img, orientations=9,  
-                                 pixels_per_cell=(8, 8), cells_per_block=(2, 2),  
+                                 pixels_per_cell=(16, 16), cells_per_block=(2, 2),  
                                  block_norm='L2-Hys', visualize=True, transform_sqrt=True)
     return hog
 
 def build_model(input_shape,model):
-    base_model = model(input_shape=input_shape,
-                                             include_top=False,#Not include the classifier
-                                             weights='imagenet')
+    base_model = model(include_top=True, input_shape=input_shape, weights='imagenet')
+    
     base_model.trainable = False
-    layer = [base_model,keras.layers.Flatten()]
-    model = tf.keras.Sequential(layer)
+    model = Model(inputs=base_model.inputs, 
+                           outputs=base_model.layers[-2].output,
+                           name="main_model")
     return model
 
 W,H = 224,224
+
 def calModel(img, model):
     img = cv.resize(img,(W,H))
     with tf.device('GPU:0'):
@@ -80,8 +84,8 @@ Softmax_vgg16 = joblib.load(open(get_model_path(Softmax_vgg16_path), 'rb'))
 SVM_vgg19 = joblib.load(open(get_model_path(SVM_vgg19_path), 'rb'))
 Softmax_vgg19 = joblib.load(open(get_model_path(Softmax_vgg19_path), 'rb'))
 
-encoderHOG = joblib.load(open(get_model_path(encoder_hog), 'rb'))
-encoderHist = joblib.load(open(get_model_path(encoder_hist), 'rb'))
+encoder = joblib.load(open(get_model_path(encoder_), 'rb'))
+
 
 
 
@@ -90,44 +94,44 @@ def predict(image_path, true_label):
     feature = extract_feature(image_path,calHOG,0)
     
     y_hat_svm_hog = SVM_hog.predict([feature.reshape(-1)])
-    y_hat_svm_hog = encoderHOG.classes_[y_hat_svm_hog]
+    y_hat_svm_hog = encoder.classes_[y_hat_svm_hog]
         
     y_hat_softmax_hog = Softmax_hog.predict([feature.reshape(-1)])
-    y_hat_softmax_hog = encoderHOG.classes_[y_hat_softmax_hog]
+    y_hat_softmax_hog = encoder.classes_[y_hat_softmax_hog]
     #------------------------------------------------------------------
     feature = extract_feature(image_path,calHist,0)
     
     y_hat_svm_hist = SVM_hist.predict([feature.reshape(-1)])
-    y_hat_svm_hist = encoderHist.classes_[y_hat_svm_hist]
+    y_hat_svm_hist = encoder.classes_[y_hat_svm_hist]
         
     y_hat_softmax_hist = Softmax_hist.predict([feature.reshape(-1)])
-    y_hat_softmax_hist = encoderHist.classes_[y_hat_softmax_hist]
+    y_hat_softmax_hist = encoder.classes_[y_hat_softmax_hist]
     #----------------------------------------------------------------------
     feature = extract_feature(image_path,calModel16,1)
     
     y_hat_svm_vgg16 = SVM_vgg16.predict([feature.reshape(-1)])
-    y_hat_svm_vgg16 = encoderHist.classes_[y_hat_svm_vgg16]
+    y_hat_svm_vgg16 = encoder.classes_[y_hat_svm_vgg16]
         
     y_hat_softmax_vgg16 = Softmax_vgg16.predict([feature.reshape(-1)])
-    y_hat_softmax_vgg16 = encoderHist.classes_[y_hat_softmax_vgg16]
+    y_hat_softmax_vgg16 = encoder.classes_[y_hat_softmax_vgg16]
     #--------------------------------------------------------------------
     feature = extract_feature(image_path,calModel19,1)
     
     y_hat_svm_vgg19 = SVM_vgg19.predict([feature.reshape(-1)])
-    y_hat_svm_vgg19 = encoderHist.classes_[y_hat_svm_vgg19]
+    y_hat_svm_vgg19 = encoder.classes_[y_hat_svm_vgg19]
         
     y_hat_softmax_vgg19 = Softmax_vgg19.predict([feature.reshape(-1)])
-    y_hat_softmax_vgg19 = encoderHist.classes_[y_hat_softmax_vgg19]
+    y_hat_softmax_vgg19 = encoder.classes_[y_hat_softmax_vgg19]
     #-------------------------------------------------------------------------------------
     table = {"Hist":{"Softmax Regression":y_hat_softmax_hist[0],"SVM":y_hat_svm_hist[0]},
             "HOG":{"Softmax Regression":y_hat_softmax_hog[0],"SVM":y_hat_svm_hog[0]},
             "VGG16":{"Softmax Regression":y_hat_softmax_vgg16[0],"SVM":y_hat_svm_vgg16[0]},
             "VGG19":{"Softmax Regression":y_hat_softmax_vgg19[0],"SVM":y_hat_svm_vgg19[0]},
         }
-    #print(f"True label:{true_label}\n\tHOG\n\t\tSoftmax predict:\t{y_hat_softmax_hog}\n\t\tSVM predict:\t\t{y_hat_svm_hog}\
-    #\n\tHist\n\t\tSoftmax predict:\t{y_hat_softmax_hist}\n\t\tSVM predict:\t\t{y_hat_svm_hist}\
-    #\n\tVGG16\n\t\tSoftmax predict:\t{y_hat_softmax_vgg16}\n\t\tSVM predict:\t\t{y_hat_svm_vgg16}\
-    #\n\tVGG19\n\t\tSoftmax predict:\t{y_hat_softmax_vgg19}\n\t\tSVM predict:\t\t{y_hat_svm_vgg19}")
+    print(f"True label:{true_label}\n\tHOG\n\t\tSoftmax predict:\t{y_hat_softmax_hog}\n\t\tSVM predict:\t\t{y_hat_svm_hog}\
+    \n\tHist\n\t\tSoftmax predict:\t{y_hat_softmax_hist}\n\t\tSVM predict:\t\t{y_hat_svm_hist}\
+    \n\tVGG16\n\t\tSoftmax predict:\t{y_hat_softmax_vgg16}\n\t\tSVM predict:\t\t{y_hat_svm_vgg16}\
+    \n\tVGG19\n\t\tSoftmax predict:\t{y_hat_softmax_vgg19}\n\t\tSVM predict:\t\t{y_hat_svm_vgg19}")
     df = pd.DataFrame(table).T
     dff = df.style.apply(lambda x: ["background-color:lime" if i==true_label else "background-color:" for i in x ])
     return dff
--- frontend.py
+++ frontend.py
@@ -5,15 +5,18 @@ import backend as democv
 tab1, tab2 = st.tabs(["Test","Demo"])
 with tab1:
     image_ip = st.file_uploader("Tải ảnh cần dự đoán loại nấm",key="data")
+    true_label = st.text_input("True label of image")
     if image_ip!=None:
         with open(f"./data/data_upload/image.jpg","wb+") as f:
             f.write(image_ip.getvalue())
-        col1, col2 = st.columns(2)
-        with col1: 
-            image = Image.open("./data/data_upload/image.jpg")
-            st.image(image)
-        with col2: 
-            st.dataframe(democv.predict("./data/data_upload/image.jpg",""))
+        button = st.button("Predict")
+        if button:
+            col1, col2 = st.columns(2)
+            with col1: 
+                image = Image.open("./data/data_upload/image.jpg")
+                st.image(image)
+            with col2: 
+                st.dataframe(democv.predict("./data/data_upload/image.jpg",true_label))
 with tab2:
     img1 = "./data/2237851965-148423.JPG"
     label1 = "Lecanoromycetes"
@@ -30,17 +33,33 @@ with tab2:
     tab1_1, tab1_2, tab1_3, tab1_4 = st.tabs([label1,label2,label3,label4])
     with tab1_1:
         col1, col2 = st.columns(2)
-        with col1: st.image(Image.open(img1),caption=label1)
-        with col2: st.dataframe(democv.predict(img1,label1))
+        with col1: 
+            st.image(Image.open(img1),caption=label1)
+            show1 = st.button("Predict", key="1")
+        with col2:
+            if show1:
+                st.dataframe(democv.predict(img1,label1))
     with tab1_2:
         col1, col2 = st.columns(2)
-        with col1: st.image(Image.open(img2),caption=label2)
-        with col2: st.dataframe(democv.predict(img2,label2))
+        with col1: 
+            st.image(Image.open(img2),caption=label2)
+            show2 = st.button("Predict", key="2")
+        with col2: 
+            if show2:
+                st.dataframe(democv.predict(img2,label2))
     with tab1_3:
         col1, col2 = st.columns(2)
-        with col1: st.image(Image.open(img3),caption=label3)
-        with col2: st.dataframe(democv.predict(img3,label3))
+        with col1: 
+            st.image(Image.open(img3),caption=label3)
+            show3 = st.button("Predict", key="3")
+        with col2: 
+            if show3:
+                st.dataframe(democv.predict(img3,label3))
     with tab1_4:
         col1, col2 = st.columns(2)
-        with col1: st.image(Image.open(img4),caption=label4)
-        with col2: st.dataframe(democv.predict(img4,label4))
+        with col1: 
+            st.image(Image.open(img4),caption=label4)
+            show4 = st.button("Predict", key="4")
+        with col2: 
+            if show4:
+                st.dataframe(democv.predict(img4,label4))
